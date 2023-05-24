5a6bd348554478d11b22e3650576119cc5e5dd57
mdsahilhussain <mdsahil.hussain7@gmail.com>
2023-05-25 00:04:53 +0530

    add hero section

--- src/components/layout-components/hero-section/HeroSection.tsx
+++ src/components/layout-components/hero-section/HeroSection.tsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./hero.models.css";
 import { Button, CommunityButton } from "../../ui-components/button/Button";
+import FreeimageDataList from "./heroimage.data.json";
 const HeroSection = () => {
+  const [imageOne, setImageOne] = useState<object | unknown>(null);
+  const [imageTwo, setImageTwo] = useState<object | unknown>(null);
+  const [imageThree, setImageThree] = useState<object | unknown>(null);
+  const [imageFour, setImageFour] = useState<object | unknown>(null);
+  const [imageFive, setImageFive] = useState<object | unknown>(null);
+  const [imageSix, setImageSix] = useState<object | unknown>(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomNumber = Math.round(
+        Math.random() * (FreeimageDataList.length - 1)
+      );
+      setImageOne(
+        FreeimageDataList?.filter((data) => data?._id === randomNumber)
+      );
+    }, 5000);
+
+    // Clean up the interval on unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  console.log(imageOne);
   return (
     <header className="hero">
       <div className="hero___headerImage">
-        <img />
-        <img />
+        <img
+          src={imageOne?.image_src}
+          className="hero___headerImage--imagesOne"
+          alt="imagesOne"
+        />
+        <img
+          src={""}
+          className="hero___headerImage--imagesTwo"
+          alt="imagesTwo"
+        />
         <div className="hero___headerImage--rectangleOne"></div>
         <div className="hero___headerImage--rectangleTwo"></div>
-        <img />
+        <img
+          src={""}
+          className="hero___headerImage--imagesThree"
+          alt="imagesThree"
+        />
       </div>
 
       <div className="hero___headerTitle">
@@ -24,17 +59,29 @@ const HeroSection = () => {
           online experience now!
         </h6>
         <div className="hero___headerTitle--buttonController">
-          <Button title="Explore"/>
+          <Button title="Explore" />
           <CommunityButton title="Community" />
         </div>
       </div>
 
       <div className="hero___headerImage">
-        <img />
-        <img />
+        <img
+          src={""}
+          className="hero___headerImage--imagesFour"
+          alt="imagesFour"
+        />
+        <img
+          src={""}
+          className="hero___headerImage--imagesFive"
+          alt="imagesFive"
+        />
         <div className="hero___headerImage--rectangleThree"></div>
         <div className="hero___headerImage--rectangleFour"></div>
-        <img />
+        <img
+          src={""}
+          className="hero___headerImage--imagesSix"
+          alt="imagesSix"
+        />
       </div>
     </header>
   );
