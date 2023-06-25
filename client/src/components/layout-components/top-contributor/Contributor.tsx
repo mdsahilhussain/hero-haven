516cbc6a1dae4cb14a8e8062d55f7cdd83e3055e
mdsahilhussain <mdsahil.hussain7@gmail.com>
2023-06-25 22:21:02 +0530

    update contributor file

--- client/src/components/layout-components/top-contributor/Contributor.tsx
+++ client/src/components/layout-components/top-contributor/Contributor.tsx
@@ -7,8 +7,6 @@ const TopContributor = () => {
     const controller = new AbortController();
     const signal = controller.signal;
 
-    
-
     return () => {
       controller.abort();
     };
@@ -20,46 +18,95 @@ const TopContributor = () => {
         Our Top <span>5</span> Contributors
       </h3>
       <div className="contributor___container">
-        <div className="contributor___container___left"></div>
-        <div className="contributor___container___right">
-          <div className="contributor___container___right--collage">
-            <div className="contributor___container___right--collage__fistLine">
-              <div className="collage___3rdPerson_Card">
-                <img src={profileImage} alt="profileImage" />
-                <h1>
-                  <span>3</span>rd rank
-                </h1>
-              </div>
-              <div className="collage__1stPerson_Card">
-                <img src={profileImage} alt="profileImage" />
-                <h1>
-                  <span>1</span>st rank
-                </h1>
-              </div>
-              <div className="collage___2ndPerson_Card">
-                <img src={profileImage} alt="profileImage" />
-                <h1>
-                  <span>2</span>nd rank
-                </h1>
-              </div>
+        <div className="contributor___container___ranker">
+          <div
+            className="contributor___container___ranker--card card_line3"
+            style={{ alignItems: "flex-start" }}
+          >
+            <div
+              className="contributor___container___ranker--card__image image_line3"
+              style={{ marginLeft: "2em" }}
+            >
+              <img src={profileImage} alt="profile_image" />
+              <span>4</span>
+              <h5>Sahil Hussain</h5>
             </div>
-            <div className="contributor___container___right--collage__secondLine">
-              <div className="collage___4thPerson_Card">
-                <img src={profileImage} alt="profileImage" />
-                <h1>
-                  <span>4</span>th rank
-                </h1>
-              </div>
-              <div className="collage___5thPerson_Card">
-                <img src={profileImage} alt="profileImage" />
-                <h1>
-                  <span>5</span>th rank
-                </h1>
+            <div className="contributor___container___ranker--card__stage">
+              <div className="top"></div>
+              <div className="front front4"></div>
+            </div>
+          </div>
+          <div
+            className="contributor___container___ranker--card card_line2"
+            style={{ alignItems: "flex-start", marginLeft: "-3.5em" }}
+          >
+            <div
+              className="contributor___container___ranker--card__image image_line2"
+              style={{ marginLeft: "2em" }}
+            >
+              <img src={profileImage} alt="profile_image" />
+              <span>2</span>
+              <h5>Sahil Hussain</h5>
+            </div>
+            <div className="contributor___container___ranker--card__stage">
+              <div className="top"></div>
+              <div className="front front2"></div>
+            </div>
+          </div>
+          <div className="contributor___container___ranker--card card1">
+            <div className="contributor___container___ranker--card__image image1">
+              <img src={profileImage} alt="profile_image" />
+              <span>1</span>
+              <h5>Sahil Hussain</h5>
+            </div>
+            <div className="contributor___container___ranker--card__stage">
+              <div className="top"></div>
+              <div className="front front1"></div>
+            </div>
+          </div>
+          <div
+            className="contributor___container___ranker--card card_line2"
+            style={{ alignItems: "flex-end", marginRight: "-3.5em" }}
+          >
+            <div
+              className="contributor___container___ranker--card__image image_line2"
+              style={{ marginRight: "2em" }}
+            >
+              <img src={profileImage} alt="profile_image" />
+              <span>3</span>
+              <h5>Sahil Hussain</h5>
+            </div>
+            <div className="contributor___container___ranker--card__stage">
+              <div className="top"></div>
+              <div className="front front3"></div>
+            </div>
+          </div>
+          <div
+            className="contributor___container___ranker--card card_line3"
+            style={{ alignItems: "flex-end" }}
+          >
+            <div
+              className="contributor___container___ranker--card__image image_line3"
+              style={{ marginRight: "2em" }}
+            >
+              <img src={profileImage} alt="profile_image" />
+              <span>5</span>
+              <h5>Sahil Hussain</h5>
+            </div>
+            <div className="contributor___container___ranker--card__stage">
+                
+              <div className="top"></div>
+              <div className="front front5">
+                sahil husa
               </div>
             </div>
           </div>
         </div>
+        <div >
+        <h1>sahil</h1>
       </div>
+      </div>
+      
     </section>
   );
 };
